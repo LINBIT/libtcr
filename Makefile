f438e7f70be71a44bdba1b8d4dbae3326ed1b101
Philipp Reisner <philipp.reisner@linbit.com>
2009-08-21 23:04:29 +0200

    tc_waitq_test: Simulating manny superfluous wakeup events....

--- Makefile
+++ Makefile
@@ -1,13 +1,16 @@
 CFLAGS=-Wall -g
 LDFLAGS=-lpthread -g
 
-all: libtc.a tc_main tc_mutex_test1
+all: libtc.a tc_main tc_mutex_test1 tc_waitq_test
 
 tc_main: main.o
 	$(CC) -o $@ $^ $(LDFLAGS) -L. -ltc 
 
 tc_mutex_test1: tc_mutex_test1.o
 	$(CC) -o $@ $^ $(LDFLAGS) -L. -ltc 
+
+tc_waitq_test: tc_waitq_test.o
+	$(CC) -o $@ $^ $(LDFLAGS) -L. -ltc
 
 clean:
 	rm -f *.o *.a
---
+++ tc_waitq_test.c
@@ -0,0 +1,45 @@
+#include "threaded_cr.h"
+#include <unistd.h>
+#include <sys/epoll.h>
+
+static int cond = 0;
+static struct tc_waitq wq;
+
+static void waker_superfluous(void *unused)
+{
+	while (1) {
+		tc_sleep(CLOCK_MONOTONIC, 0, 1000); /* 1us, as sched yield() .. */
+		// printf("waking\n");
+		tc_waitq_wakeup(&wq);
+	}
+}
+
+static void waker_valid(void *unused)
+{
+	while (1) {
+		tc_sleep(CLOCK_MONOTONIC, 0, 100000000); /* 100ms */
+		cond = 1;
+		tc_waitq_wakeup(&wq);
+	}
+}
+
+static void starter(void *unused)
+{
+	tc_waitq_init(&wq);
+	tc_thread_new(waker_valid, NULL, "waker_valid");
+	tc_thread_new(waker_superfluous, NULL, "waker_superfluous");
+
+	while (1) {
+		tc_waitq_wait_event(&wq, cond == 1);
+		printf("condition got true\n");
+		cond = 0;
+	}
+}
+
+
+int main()
+{
+	tc_run(starter, NULL, "test", 4);
+	sleep(60);
+	return 0;
+}
