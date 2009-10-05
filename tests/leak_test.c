4f51a0e106e39de3721767d76aa6d70850b54c35
Philipp Reisner <philipp.reisner@linbit.com>
2009-10-05 13:34:04 +0200

    Added leak_test.c

---
+++ tests/leak_test.c
@@ -0,0 +1,64 @@
+// #include "drbd-proxy.h"
+#include <tc/threaded_cr.h>
+#include <tc/atomic.h>
+// #include "helpers.h"
+// #include "control.h"
+#include <sys/epoll.h>
+#include <syslog.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <sys/queue.h>
+#include <semaphore.h>
+#include <arpa/inet.h>
+
+
+struct tc_signal the_drbd_signal;
+struct tc_signal the_signal;
+
+void drbd_connection(void *unused)
+{
+	static int thread_no = 0;
+	int my_thread_no = thread_no++;
+
+	fprintf(stderr, "DRBD reader %d started.\n", my_thread_no);
+
+	tc_signal_enable(&the_drbd_signal);
+	tc_signal_enable(&the_signal);
+
+	tc_sleep(CLOCK_MONOTONIC, 0, 10000000);
+
+	fprintf(stderr, "%d: ending DRBD connection\n", my_thread_no);
+
+	tc_signal_disable(&the_signal);
+	tc_signal_disable(&the_drbd_signal);
+
+	tc_signal_fire(&the_drbd_signal);
+	//tc_signal_fire(&the_drbd_signal);
+}
+
+
+static void starter(void *unused)
+{
+	struct tc_threads threads;
+
+	tc_signal_init(&the_drbd_signal);
+	tc_signal_init(&the_signal);
+
+	while (1) {
+		tc_threads_new(&threads, drbd_connection, NULL, "DRBD conn");
+		fprintf(stderr, "into tc_threads_wait\n");
+		tc_threads_wait(&threads);
+		fprintf(stderr, "out of tc_threads_wait\n");
+
+		tc_sleep(CLOCK_MONOTONIC, 0, 10000000);
+	}
+}
+
+
+int main()
+{
+	tc_run(starter, NULL, "test", 10);
+	return 0;
+}
