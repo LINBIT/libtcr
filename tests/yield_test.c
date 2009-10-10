6d7112638c52757bf849aeabc0835fcc1d9e45e7
Philipp Reisner <philipp.reisner@linbit.com>
2009-10-10 11:33:39 +0200

    A test program for the iwi/yield code

---
+++ tests/yield_test.c
@@ -0,0 +1,31 @@
+#include <tc/threaded_cr.h>
+#include <tc/atomic.h>
+
+struct tc_signal the_drbd_signal;
+struct tc_signal the_signal;
+
+static void func(void *str)
+{
+	int i;
+
+	for (i=0; i<100; i++)
+		printf("%d %s\n", i, (char *)str);
+}
+
+static void starter(void *unused)
+{
+	struct tc_thread *a, *b;
+
+	a = tc_thread_new(func, "a", "func_a");
+	b = tc_thread_new(func, "b", "func_b");
+
+	tc_thread_wait(a);
+	tc_thread_wait(b);
+}
+
+
+int main()
+{
+	tc_run(starter, NULL, "test", 2);
+	return 0;
+}
