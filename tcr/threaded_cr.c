1a3b15db6e539d9b5f7dfa9016af6703654c3b13
Philipp Reisner <philipp.reisner@linbit.com>
2010-06-07 12:04:08 +0200

    Adding some comments pointing to LGPL 3

--- tcr/threaded_cr.c
+++ tcr/threaded_cr.c
@@ -1,3 +1,22 @@
+/*
+   This file is part of libtcr by Philipp Reisner.
+
+   Copyright (C) 2009-2010, LINBIT HA-Solutions GmbH.
+
+   drbd is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation; version 3.
+
+   libtcr is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with drbd; see the file COPYING.  If not, write to
+   the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #define _GNU_SOURCE /* for asprintf() */
 
 #include <sys/epoll.h>
