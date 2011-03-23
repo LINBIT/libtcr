e1347042b1c7c81fa8d341f4bf3bebac42a73a5e
Philipp Marek <philipp.marek@linbit.com>
2011-03-23 11:54:25 +0000

    Reduce debug output in the tests.

--- tests/leak_test.c
+++ tests/leak_test.c
@@ -1,8 +1,5 @@
-// #include "drbd-proxy.h"
 #include <tcr/threaded_cr.h>
 #include <tcr/atomic.h>
-// #include "helpers.h"
-// #include "control.h"
 #include <sys/epoll.h>
 #include <syslog.h>
 #include <unistd.h>
@@ -19,24 +16,19 @@ struct tc_signal the_signal;
 
 void drbd_connection(void *unused)
 {
-	static int thread_no = 0;
-	int my_thread_no = thread_no++;
 	struct tc_signal_sub *ed, *es;
 
-	fprintf(stderr, "DRBD reader %d started.\n", my_thread_no);
 
 	ed = tc_signal_subscribe(&the_drbd_signal);
 	es = tc_signal_subscribe(&the_signal);
 
 	tc_sleep(CLOCK_MONOTONIC, 0, 10000000);
 
-	fprintf(stderr, "%d: ending DRBD connection\n", my_thread_no);
 
 	tc_signal_unsubscribe(&the_signal, ed);
 	tc_signal_unsubscribe(&the_drbd_signal, es);
 
 	tc_signal_fire(&the_drbd_signal);
-	//tc_signal_fire(&the_drbd_signal);
 }
 
 
@@ -50,10 +42,9 @@ static void starter(void *unused)
 
 	for(i=0; i<1000; i++)
 	{
+		putchar(i & 0x3f ? '.' : '\n');
 		tc_thread_pool_new(&threads, drbd_connection, NULL, "DRBD conn %d", 0);
-		fprintf(stderr, "into tc_thread_pool_wait\n");
 		tc_thread_pool_wait(&threads);
-		fprintf(stderr, "out of tc_thread_pool_wait\n");
 
 		tc_sleep(CLOCK_MONOTONIC, 0, 10000000);
 	}
--- tests/leak_test2.c
+++ tests/leak_test2.c
@@ -1,8 +1,5 @@
-// #include "drbd-proxy.h"
 #include <tcr/threaded_cr.h>
 #include <tcr/atomic.h>
-// #include "helpers.h"
-// #include "control.h"
 #include <sys/epoll.h>
 #include <syslog.h>
 #include <unistd.h>
@@ -19,32 +16,27 @@ struct tc_signal the_signal;
 
 void drbd_connection(void *unused)
 {
-	static int thread_no = 0;
-	int my_thread_no = thread_no++;
 	struct tc_signal_sub *ed, *es;
 
-	fprintf(stderr, "DRBD reader %d started: %p.\n", my_thread_no, tc_current());
 
 	ed = tc_signal_subscribe(&the_drbd_signal);
 	es = tc_signal_subscribe(&the_signal);
 
 	tc_sleep(CLOCK_MONOTONIC, 0, 10000000);
 
-	fprintf(stderr, "%d: ending DRBD connection\n", my_thread_no);
 
 	tc_signal_unsubscribe(&the_signal, es);
 	tc_signal_unsubscribe(&the_drbd_signal, ed);
 
 	tc_signal_fire(&the_drbd_signal);
-	//tc_signal_fire(&the_drbd_signal);
 }
 
 
 void writer(void *unused)
 {
-	fprintf(stderr, "starting writer\n");
+	fprintf(stderr, "starting writer %p\n", tc_current());
 	tc_sleep(CLOCK_MONOTONIC, 0, 100000000);
-	fprintf(stderr, "Ending writer\n");
+	fprintf(stderr, "Ending writer %p\n", tc_current());
 }
 
 void accepter(void *unused)
@@ -52,7 +44,6 @@ void accepter(void *unused)
 	struct tc_thread *the_writer;
 	struct tc_signal_sub *ss;
 
-	fprintf(stderr, "starting accepter\n");
 	ss = tc_signal_subscribe(&the_drbd_signal);
 
 	the_writer = tc_thread_new(writer, NULL, "writer");
@@ -60,7 +51,6 @@ void accepter(void *unused)
 		fprintf(stderr, "RV_INTR in tc_thread_wait(%p)\n", the_writer);
 	}
 	tc_signal_unsubscribe(&the_drbd_signal, ss);
-	fprintf(stderr, "ending accepter\n");
 }
 
 
@@ -76,17 +66,15 @@ static void starter(void *unused)
 
 	for(i=0; i<3000; i++)
 	{
+		fprintf(stderr, "new round\n");
 		the_accepter = tc_thread_new(accepter, NULL, "accepter");
 		tc_thread_pool_new(&threads, drbd_connection, NULL, "DRBD conn %d", 0);
-		fprintf(stderr, "into tc_thread_pool_wait\n");
 		tc_thread_pool_wait(&threads);
-		fprintf(stderr, "out of tc_thread_pool_wait\n");
 
-		fprintf(stderr, "into tc_thread_wait accepter\n");
 		tc_thread_wait(the_accepter);
-		fprintf(stderr, "out of tc_thread_wait accepter\n");
 
 		tc_sleep(CLOCK_MONOTONIC, 0, 10000000);
+		fflush(NULL);
 	}
 }
 
