05522ac624984145d2289b35ae557df6d01a139e
Lars Ellenberg <lars.ellenberg@linbit.com>
2015-04-15 00:08:50 +0200

    tests: fix multiple-domains.c to not initially sleep 5 minutes
    Also print out the final counter values for both tc scheduler domains.

--- tests/multiple-domains.c
+++ tests/multiple-domains.c
@@ -4,12 +4,13 @@
 
 #include "tcr/threaded_cr.h"
 
-static int go;
+static volatile int go;
 static struct tc_waitq wq;
 
 struct per_sched_t {
 	atomic_t cnt;
 	struct tc_mutex m;
+	const unsigned char pad[128 - sizeof(atomic_t) - sizeof(struct tc_mutex)];
 };
 
 struct per_sched_t per_sched[2] = {  };
@@ -25,6 +26,7 @@ void worker(void *_a)
 	struct per_sched_t *ps = _a;
 
 	int this_worker_no = atomic_inc(&worker_no);
+	int64_t a;
 
 	fprintf(stdout, "worker %d started (%p), for %p.\n", this_worker_no, tc_current(), ps);
 	tc_waitq_wait_event(&wq, go);
@@ -32,7 +34,11 @@ void worker(void *_a)
 	while (go) {
 		tc_mutex_lock(&ps->m);
 		atomic_inc(&ps->cnt);
+		a = atomic_read(&ps->cnt);
 		tc_mutex_unlock(&ps->m);
+		if ((a & 0xfffffL) == 0) {
+			fprintf(stdout, "a: %lu\n", a);
+		}
 	}
 }
 
@@ -56,18 +62,20 @@ void starter(void *unused)
 	tc_thread_pool_new_in_domain(&t2, worker, &per_sched[1], "worker2", 0, sched2);
 	tc_renice_domain(sched2, 10);
 
-	tc_sleep(CLOCK_MONOTONIC, 0, 300e9);
-
+	tc_sleep(CLOCK_MONOTONIC, 1, 0);
 	go = 1;
 	tc_waitq_wakeup_all(&wq);
 
-	tc_sleep(CLOCK_MONOTONIC, 3, 0);
+	tc_sleep(CLOCK_MONOTONIC, 10, 0);
 	go = 0;
 
 	tc_thread_pool_wait(&t1);
 	tc_thread_pool_wait(&t2);
 
 	fprintf(stdout, "ending starter.\n");
+
+	printf("m[0]: %lu\n", atomic_read(&per_sched[0].cnt));
+	printf("m[1]: %lu\n", atomic_read(&per_sched[1].cnt));
 }
 
 
